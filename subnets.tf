7150627df3ca6c3bc1e889e9602f3854ab98b9c1
root <root@DESKTOP-6UKUQHE.localdomain>
2025-03-21 16:25:39 +0000

    public and private subnets added

---
+++ subnets.tf
@@ -0,0 +1,22 @@
+resource "aws_subnet" "public" {
+  count                   = length(var.subnets_public)
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.subnets_public[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+      tags = {
+    Name = "subnet-public-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count      = length(var.subnets_private)
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = var.subnets_private[count.index]
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+    tags = {
+    Name = "subnet-private-${count.index + 1}"
+  }
+}
+
+data "aws_availability_zones" "available" {}
