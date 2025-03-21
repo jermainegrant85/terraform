4124d8d7543c80ef35ea576b4cd4c8cb2f7bead9
root <root@DESKTOP-6UKUQHE.localdomain>
2025-03-21 16:33:52 +0000

    routing for VPC subnets added

---
+++ routes.tf
@@ -0,0 +1,25 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.internet_gw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main_vpc.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
