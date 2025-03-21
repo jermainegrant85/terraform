e97169fd73bd57d90f3fc02b608327518fd1acc4
root <root@DESKTOP-6UKUQHE.localdomain>
2025-03-21 16:20:36 +0000

    AWS VPC/IG tags added

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,18 @@
 resource "aws_vpc" "main_vpc" {
   cidr_block = var.vpc_cidr
+    tags = {
+    Name        = "main-vpc"
+    Environment = "dev"
+    Project     = "terraform-iac-challenge"
+  }
 }
 
 resource "aws_internet_gateway" "internet_gw" {
   vpc_id = aws_vpc.main_vpc.id
+      tags = {
+    Name        = "main-vpc-ig"
+    Environment = "dev"
+    Project     = "terraform-iac-challenge"
+  }
+
 }
