333d238d41c38032348f76f68ba25d768e11924a
root <root@DESKTOP-6UKUQHE.localdomain>
2025-03-21 16:39:12 +0000

    tagging for routes and SG

--- routes.tf
+++ routes.tf
@@ -1,25 +1,50 @@
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main_vpc.id
+        tags = {
+    Name        = "main-vpc-rt-public"
+    Environment = "dev"
+    Project     = "terraform-iac-challenge"
+  }
 }
 
 resource "aws_route" "public_internet_access" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.internet_gw.id
+          tags = {
+    Name        = "main-vpc-route-public"
+    Environment = "dev"
+    Project     = "terraform-iac-challenge"
+  }
 }
 
 resource "aws_route_table_association" "public" {
   count          = length(aws_subnet.public)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+          tags = {
+    Name        = "main-vpc-rt-association-public"
+    Environment = "dev"
+    Project     = "terraform-iac-challenge"
+  }
 }
 
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main_vpc.id
+          tags = {
+    Name        = "main-vpc-rt-private"
+    Environment = "dev"
+    Project     = "terraform-iac-challenge"
+  }
 }
 
 resource "aws_route_table_association" "private" {
   count          = length(aws_subnet.private)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
+            tags = {
+    Name        = "main-vpc-rt-association-private"
+    Environment = "dev"
+    Project     = "terraform-iac-challenge"
+  }
 }
---
+++ security_groups.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "web_access" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+      tags = {
+    Name        = "web-access-sg"
+    Environment = "dev"
+    Project     = "terraform-iac-challenge"
+  }
+
+}
